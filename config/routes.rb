6a490c145b1a9be825a61aec32578e29196d826e
Demian Yovanovich <subwaydemiany@gmail.com>
2022-10-01 15:36:42 -0300

    creado el CRUD de products

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :products
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
